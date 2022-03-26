490beb4bec443c90f84c04a3a7bd9b493d65793f
Buelahabi <buelah007@gmail.com>
2022-03-26 12:56:20 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+import Landing from './components/Landing';
+import Clients from './components/Clients';
+import Actions from './components/Actions';
+import Analytics from './components/Analytics';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <div id="nav-bar">
+            <Link to="/" style={{ textDecoration: 'none'}}><div className="nav-bar-tab">Home</div> </Link>
+            <Link to="/clients" style={{ textDecoration: 'none'}}> <div className="nav-bar-tab">Clients</div> </Link>
+            <Link to="/actions" style={{ textDecoration: 'none'}}>  <div className="nav-bar-tab">Actions</div> </Link>
+            <Link to="/analytics" style={{ textDecoration: 'none'}}> <div className="nav-bar-tab"> Analytics</div> </Link>
+          </div>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/clients" render={() => <Clients />} />
+          <Route exact path="/actions" render={() => <Actions />} />
+          <Route exact path="/analytics" render={() => <Analytics />} />
+        </div>
+      </Router>
+
+)
+}
+
 }
 
 export default App;
+
+        
